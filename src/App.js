2dafbb8e5990ee97977a2b1bd995ad826cdf9b1c
sin-ha <harsh19112000@gmail.com>
2020-05-24 21:19:51 +0530

    Components 2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 
 import {Navbar,NavbarBrand} from "reactstrap";
 import Menu from "./components/MenuComponent";
+import {DISHES} from "./shared/dishes";
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary" >
-        <div className="container">
-          <NavbarBrand href ="/">Ristorante Con Fusionas</NavbarBrand>
-        </div>
-      </Navbar>
-        <Menu />
-    </div>
-  );
+class App extends Component {
+    render() {
+
+
+        return (
+            <div>
+                <Navbar dark color="primary">
+                    <div className="container">
+                        <NavbarBrand href="/">Ristorante Con Fusionas</NavbarBrand>
+                    </div>
+                </Navbar>
+                <Menu dishes={DISHES}/>
+            </div>
+        );
+    }
+
 }
 
 export default App;
