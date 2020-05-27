864652e46893ca9441945737aed2dac3ebaedf06
sin-ha <harsh19112000@gmail.com>
2020-05-28 01:31:12 +0530

    single page components

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,10 +3,15 @@ import {Switch,Route, Redirect} from "react-router-dom";
 
 import Menu from "../components/MenuComponent";
 import DishDetail from "./DishDetailComponent";
-import {DISHES} from "../shared/dishes";
 import HeaderComponent from "./HeaderComponent";
 import FooterComponent from "./FooterComponent";
 import HomeComponent from "./HomeComponent";
+import ContactUs from "./ContactUsComponent";
+
+import {DISHES} from "../shared/dishes";
+import {LEADERS} from "../shared/leaders";
+import {COMMENTS} from "../shared/comments";
+import {PROMOTIONS} from "../shared/promotions";
 
 
 class Main extends Component {
@@ -14,6 +19,9 @@ class Main extends Component {
         super(props);
         this.state={
             dishes : DISHES,
+            leaders:LEADERS,
+            promotions:PROMOTIONS,
+            comments:COMMENTS
         }
     }
 
@@ -21,7 +29,13 @@ class Main extends Component {
 
         const HomePage = ()=>{
             return(
-                <HomeComponent/>
+                <HomeComponent
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
+
+
             )
         }
 
@@ -29,7 +43,8 @@ class Main extends Component {
             <div>
                 <HeaderComponent />
                 <Switch>
-                    <Route path='/home' component={HomePage}/>
+                    <Route path="/home" component={HomePage}/>
+                    <Route exact path='/contactus' component={ContactUs}/>
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
                     <Redirect to="/home" />
                 </Switch>
